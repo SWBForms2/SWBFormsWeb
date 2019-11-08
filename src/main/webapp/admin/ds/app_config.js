57d3a28efbfa2c580b2b0a4f73071fa7025427a5
Javier Solis <softjei@gmail.com>
2019-11-07 22:51:45 -0600

    Update SmartClient 12 ContextPaths ContextBox HelpText Compression DS

--- src/main/webapp/admin/ds/app_config.js
+++ src/main/webapp/admin/ds/app_config.js
@@ -1,6 +1,8 @@
 _modelid = "SWBForms";
 _dataStore = "mongodb";
+//_dataStore = "ts_leveldb";
 roles={prog:"Programador", su:"Super User",admin:"Administrator",user:"User"};
 eng.config.appName="SWBForms";
 eng.config.appURL="https://github.com/SWBForms2";
-eng.config.appSkin="skin-green";
+eng.config.appSkin="skin-blue";
+eng.config.compileDS=false;                                         //compile admin_db.js
